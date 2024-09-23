50bb237a53732bfb2ac0daed74ff6b66ceb701f4
Yukine <devyukine@gmx.de>
2024-09-23 22:36:57 +0200

    feat(commands): add toggle_update command

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -1,27 +1,60 @@
+use crate::abstraction::command::{CommandContext, CommandData, CommandError, CommandResult};
+use lazy_static::lazy_static;
+use log::warn;
 use poise::Command;
-use serenity::Error;
+use serenity::all::RoleId;
+use std::str::FromStr;
 
-pub struct UserData {
-	client: reqwest::Client,
+lazy_static! {
+	static ref UPDATE_ROLE_ID: String =
+		std::env::var("DISCORD_RETROREALM_UPDATE_ROLE_ID").unwrap_or_default();
 }
 
-impl Default for UserData {
-	fn default() -> Self {
-		Self {
-			client: reqwest::Client::new(),
-		}
-	}
-}
-
-type Context<'a> = poise::Context<'a, UserData, Error>;
-
-pub fn get_commands() -> Vec<Command<UserData, Error>> {
-	vec![ping()]
+pub fn get_commands() -> Vec<Command<CommandData, CommandError>> {
+	vec![ping(), toggle_update_role()]
 }
 
 /// Responds with pong!
-#[poise::command(slash_command)]
-async fn ping(ctx: Context<'_>) -> Result<(), Error> {
+#[poise::command(slash_command, required_permissions = "SEND_MESSAGES")]
+async fn ping(ctx: CommandContext<'_>) -> CommandResult {
 	ctx.say("pong!").await?;
+	Ok(())
+}
+
+/// Applies or removes the updates role for this user
+#[poise::command(
+	slash_command,
+	rename = "toggle_update",
+	required_permissions = "SEND_MESSAGES | MANAGE_ROLES"
+)]
+async fn toggle_update_role(ctx: CommandContext<'_>) -> CommandResult {
+	if UPDATE_ROLE_ID.is_empty() {
+		ctx.say("This command is not configured").await?;
+		warn!("UPDATE_ROLE_ID is not set, toggle_update_role command cannot be used!");
+		return Ok(());
+	}
+
+	let member = ctx.author_member().await;
+
+	if member.is_none() {
+		ctx.say("This command can only be run inside of RetroRealm's Server")
+			.await?;
+		return Ok(());
+	}
+
+	let member = member.unwrap_or_default(); // safe to unwrap because we checked above
+
+	let role_id = &RoleId::from_str(&UPDATE_ROLE_ID)?;
+
+	if member.roles.contains(role_id) {
+		member.remove_role(ctx, role_id).await?;
+
+		ctx.say("Update Role Removed!").await?;
+		return Ok(());
+	}
+
+	member.add_role(ctx, role_id).await?;
+	ctx.say("Update Role Added!").await?;
+
 	Ok(())
 }
